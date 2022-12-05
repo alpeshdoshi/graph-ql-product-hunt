ecef4e9f8c0ffac6ba6e75f3e2558471e11361b1
alpesh doshi <alpeshdoshi428@gmail.com>
2022-12-05 11:38:20 -0600

    nested-resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,6 +5,7 @@ const productsData = [
         url: 'https://educative.io/',
         numberOfVotes: 10,
         publishedAt: '2021-04-05',
+        authorId: '1',
       },
       {
         name: 'Apollo',
@@ -12,6 +13,7 @@ const productsData = [
         url: 'https://www.apollographql.com/',
         numberOfVotes: 5,
         publishedAt: '2021-01-08',
+        authorId: '2',
       },
       {
         name: 'OneGraph',
@@ -19,6 +21,27 @@ const productsData = [
         url: 'https://www.onegraph.com',
         numberOfVotes: 5,
         publishedAt: '2020-08-22',
+        authorId: '1',
+      },
+]
+/*
+ we could have embedded userData object in productsData doing that way would have saved effort in updating resolvers to add new resolver. 
+ but embedding an object within another object is not a good practice. If we want to make a change in nested object then we would need to change all parent objects where updated object is embedded.
+ Instead, better approach is to seperate the objects out- like in this case we an object for Product and User, this way if we need to update any object in future then we only update it in one place.
+ with seperate objects we can refer id of one object into another instead of embedding full object- like in this case we embedded autherId in ProductsData..
+ we will add a resolver to let Appolo Server know how to fetch user data when allProducts query is called. 
+ think of 2 seperate objects like having 2 seperate collections in MongoDB or 2 seperate tables in relational DB 
+ */
+const userData = [
+    {
+        id: '1',
+        userName: 'ellen',
+        fullName: 'Ellen James'
+      },
+      {
+        id: '2',
+        userName: 'peter',
+        fullName: 'Peter Miles'
       },
 ]
 
@@ -30,6 +53,15 @@ const resolvers = {
       allProducts: () => {
         return productsData
      },
+  },
+
+   // Specifies how to get fields for the "Product" type
+   Product: {
+    //Each function in this object specifies how to get data for fields queried on the Product type. 
+    //In this case we're only defining function to fetch author for a product
+    author: (product) => {
+        return userData.find(user => user.id===product.authorId)
+      },
   },
 }
   
